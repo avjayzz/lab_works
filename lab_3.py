468cffc5f859aadb5a2e4160f72e48936a3f9001
avjayzz <kiriktritenbrayt@gmail.com>
2026-09-09 16:56:11 +0300

    problem 3

---
+++ lab_3.py
@@ -0,0 +1,31 @@
+#--------------------------- LIBRARIES
+from random import randint
+#--------------------------- VARIABLES
+dates_list = []
+#--------------------------- FUNCTIONS
+def generate_dates(how_much):
+    for _ in range(how_much):
+        date = randint(1, 50)
+        month = randint(1, 20)
+        year = randint(1, 5000)
+        dates_list.append(str(date) + "." + str(month) + "." + str(year))
+    return True
+
+def check_dates(dates):
+    valid_dates = []
+    invalid_dates = []
+    for current in dates:
+        under_inspection = current.split('.')
+        if int(under_inspection[0]) <= 31 and int(under_inspection[1]) <= 12 and int(under_inspection[2]) <= 2026:
+            valid_dates.append(current)
+        else:
+            invalid_dates.append(current)
+            continue
+
+    return valid_dates, invalid_dates
+#---------------------------
+how_much_dates = int(input("Сколько дат сгенерировать и проверить?"))
+
+generate_dates(how_much_dates)
+answer = check_dates(dates_list)
+print(f'Количество дат прошедших проверку: {len(answer[0])}, количество дат не прошедших проверку: {len(answer[1])}')
