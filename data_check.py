d568ca567bc4f9fe8f59b85819d1f1f798e254ef
avjayzz <kiriktritenbrayt@gmail.com>
2026-09-14 00:46:41 +0300

    Update data_check.py

--- data_check.py
+++ data_check.py
@@ -1,35 +1,41 @@
-#--------------------------- LIBRARIES
+# --------------------------- LIBRARIES
 from random import randint
-#--------------------------- VARIABLES
+from datetime import datetime
+# --------------------------- VARIABLES
 dates_list = []
-#--------------------------- FUNCTIONS
+# --------------------------- FUNCTIONS
 def generate_dates(how_much):
     for _ in range(how_much):
         date = randint(1, 50)
         month = randint(1, 20)
         year = randint(1, 5000)
-        dates_list.append(str(date) + "." + str(month) + "." + str(year))
+        dates_list.append(f"{date}.{month}.{year}")
     return True
+
 
 def check_dates(dates):
     valid_dates = []
     invalid_dates = []
-    
+
     if not dates:
         print("Ошибка, укажите число большее 0!")
         return [[], []]
-    
+
     for current in dates:
-        under_inspection = current.split('.')
-        if int(under_inspection[0]) <= 31 and int(under_inspection[1]) <= 12 and int(under_inspection[2]) <= 2026:
-            valid_dates.append(current)
-        else:
+        try:
+            parsed_time = datetime.strptime(current, "%d.%m.%Y")
+            if parsed_time.year <= 2026:
+                valid_dates.append(current)
+            else:
+                invalid_dates.append(current)
+        except ValueError:
             invalid_dates.append(current)
-            continue
 
     return valid_dates, invalid_dates
-#---------------------------
-how_much_dates = int(input("Сколько дат сгенерировать и проверить?"))
+
+
+# ---------------------------
+how_much_dates = int(input("Сколько дат сгенерировать и проверить? "))
 
 generate_dates(how_much_dates)
 answer = check_dates(dates_list)
