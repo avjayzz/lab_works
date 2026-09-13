6b717042c506e45af2f21392eaa5ddf5d24936c1
avjayzz <kiriktritenbrayt@gmail.com>
2026-09-14 00:31:51 +0300

    Update quadratic_solvation.py

--- quadratic_solvation.py
+++ quadratic_solvation.py
@@ -8,7 +8,7 @@ c = int(input("Введите коэффициент C:"))
 def solve_quadratic_diskriminant():
     discriminant = b**2 - (4 * a * c)
     if discriminant < 0: return print("Нет решения в действительных числах")
-    elif discriminant == 0: return print(f'Единственным корнем уравнения является {-b * 2 * a}')
+    elif discriminant == 0: return print(f'Единственным корнем уравнения является {-b / (2 * a)}')
     else: return print(f'Первый корень уравнения: {(-b + sqrt(discriminant)) / 2 * a}, второй корень: {(-b - sqrt(discriminant)) / 2 * a}')
 #---------------------------
 while True:
