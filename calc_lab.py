772a1ed6ec0821d44c19edac16d6ca75787c03ad
avjayzz <kiriktritenbrayt@gmail.com>
2026-09-14 11:25:10 +0300

    Update calc_lab.py

--- calc_lab.py
+++ calc_lab.py
@@ -1,42 +1,36 @@
-#--------------------------- LIBRARIES
+# --------------------------- LIBRARIES
 from random import randint
-from math import prod
 import statistics as stat
-#--------------------------- VARIABLES
-numbers = []
-#--------------------------- FUNCTIONS
-def generate_random_numbers(count):
-    return [randint(1, 10000) for _ in range(count)]
-
 def stats_calculator(args, mode="basic"):
+    if not args or mode not in ["basic", "advanced", "scientific"]:
+        return None
 
-   if not args or mode not in ["basic", "advanced", "scientific"]:
-       return None
+    result = {
+        "Максимальное число": max(args),
+        "Минимальное число": min(args),
+        "Среднее арифметическое": stat.mean(args)
+    }
 
-   result = {
-       "Максимальное число": max(args),
-       "Минимальное число": min(args),
-       "Среднее арифметическое": stat.mean(args)
-   }
+    if mode in ["advanced", "scientific"]:
+        result["Медиана"] = stat.median(args)
+        result["Мода"] = stat.mode(args) # исключения
 
-   if mode in ["advanced", "scientific"]:
-       result["Медиана"] = stat.median(args)
-       result["Мода"] = stat.mode(args)
+    if mode == "scientific":
+        result["Среднее геометрическое"] = stat.geometric_mean(args) # исключение
+        result["Среднее гармоническое"] = stat.harmonic_mean(args)
 
-   if mode == "scientific":
-       result["Среднее геометрическое"] = stat.geometric_mean(args)
-       result["Среднее гармоническое"] = stat.harmonic_mean(args)
+    return result
 
-   return result
-#---------------------------
+
+# ---------------------------
 try:
-    answer = int(input("Введите количество чисел для совершения операций: "))
+    answer = int(input("Введите количество чисел для совершения операций: ")) # закинуть в соло блок трай
     mode_client = input("Введите режим использования калькулятора (basic/advanced/scientific): ").strip().lower()
 
     if answer <= 0:
         print("Количество чисел должно быть больше нуля.")
     else:
-        numbers = generate_random_numbers(answer)
+        numbers = [randint(1, 10000) for _ in range(answer)]
         final = stats_calculator(args=numbers, mode=mode_client)
 
         if final:
@@ -45,5 +39,5 @@ try:
         else:
             print("Нет данных/ неверно указан режим")
 
-except ValueError:
+except ValueError: # почитать про эксепт
     print("Некорректно введено число")
