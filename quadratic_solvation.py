cdaa7f594b432c0d224cd010cd7c0c3a662c2423
avjayzz <kiriktritenbrayt@gmail.com>
2026-09-14 11:24:43 +0300

    Update quadratic_solvation.py

--- quadratic_solvation.py
+++ quadratic_solvation.py
@@ -1,11 +1,14 @@
-#--------------------------- LIBRARIES
+# --------------------------- LIBRARIES
 from math import sqrt
-#--------------------------- VARIABLES
-a = int(input("Введите коэффициент A:"))
+
+# --------------------------- VARIABLES
+a = int(input("Введите коэффициент A:")) # проверка на число
 b = int(input("Введите коэффициент B:"))
 c = int(input("Введите коэффициент C:"))
-#--------------------------- FUNCTIONS
-def solve_quadratic_diskriminant():
+
+
+# --------------------------- FUNCTIONS
+def solve_via_diskriminant(): # название
     if a == 0:
         if b != 0:
             print(f"Уравнение линейное. Единственный корень: {-c / b}")
@@ -13,12 +16,15 @@ def solve_quadratic_diskriminant():
             print("X - любое число")
         return
 
-    discriminant = b**2 - (4 * a * c)
+    discriminant = b ** 2 - (4 * a * c)
     if discriminant < 0:
         print("Нет решения в действительных числах")
     elif discriminant == 0:
         print(f'Единственным корнем уравнения является {-b / (2 * a)}')
     else:
-        print(f'Первый корень уравнения: {(-b + sqrt(discriminant)) / (2 * a)}, второй корень: {(-b - sqrt(discriminant)) / (2 * a)}')
-#---------------------------
-solve_quadratic_diskriminant()
+        print(
+            f'Первый корень уравнения: {(-b + sqrt(discriminant)) / (2 * a)}, второй корень: {(-b - sqrt(discriminant)) / (2 * a)}')
+
+
+# ---------------------------
+solve_via_diskriminant()
